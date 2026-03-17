b9b525e79dee4bb11b505e4f25e60845ca08f744
shofiahmed69 <frex19shofi@gmail.com>
2026-03-18 03:06:43 +0600

    feat: update logo and favicon to match geometric design from image

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -7,27 +7,18 @@ export default function Logo({
 }: { className?: string }) {
     return (
         <svg
-            viewBox="0 0 100 120"
+            viewBox="0 0 100 100"
             className={className}
             xmlns="http://www.w3.org/2000/svg"
         >
-            {/* Geometric J with arrow cut */}
-            {/* Top horizontal bar */}
-            <rect x="15" y="8" width="70" height="18"
-                fill="#F97316" rx="2" />
-            {/* Right vertical stem */}
-            <rect x="67" y="8" width="18" height="80"
-                fill="#F97316" rx="2" />
-            {/* Bottom curve left */}
-            <rect x="15" y="88" width="70" height="18"
-                fill="#F97316" rx="2" />
-            {/* Left small piece */}
-            <rect x="15" y="70" width="18" height="36"
-                fill="#F97316" rx="2" />
-            {/* Arrow/slash cut - white diagonal */}
-            <polygon
-                points="20,35 42,35 72,85 50,85"
-                fill="white" />
+            <g fill="#F97316">
+                {/* Main J body */}
+                <path d="M42,15 H85 V85 H55 V70 H70 V30 H52 Z" />
+                {/* Hook/Left part */}
+                <path d="M25,65 H45 V85 H25 Z" />
+            </g>
+            {/* Signal Arrow Cut (White) */}
+            <polygon points="30,90 75,35 55,75 40,90" fill="white" />
         </svg>
     )
 }
