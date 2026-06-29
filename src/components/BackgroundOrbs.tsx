09902176d7dd6d593db1ec272e6d052a8ad51ff6
Morsalin Islam Alvee <145377186+Alvee0033@users.noreply.github.com>
2026-06-29 15:11:57 +0600

    style: enable bottom background orb visibility on mobile screens

--- src/components/BackgroundOrbs.tsx
+++ src/components/BackgroundOrbs.tsx
@@ -3,7 +3,7 @@ export default function BackgroundOrbs() {
         <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none">
             <div className="orb orb-animate absolute top-[-10%] left-[-10%] w-[600px] h-[600px] bg-orange-200/40" />
             <div
-                className="orb orb-animate absolute bottom-[-10%] right-[-5%] w-[800px] h-[800px] bg-blue-100/30 hidden md:block"
+                className="orb orb-animate absolute bottom-[-10%] right-[-5%] w-[800px] h-[800px] bg-blue-100/30"
                 style={{ animationDelay: "-5s" }}
             />
             <div
